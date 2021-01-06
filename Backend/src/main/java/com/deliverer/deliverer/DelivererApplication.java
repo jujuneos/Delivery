2416f5b98f463f2b2503cf8628fc4a71ebfa4aa6
jujuneos <lcjuniorconceicao@gmail.com>
2021-01-06 09:38:23 -0300

    Projeto criado

---
+++ Backend/src/main/java/com/deliverer/deliverer/DelivererApplication.java
@@ -0,0 +1,13 @@
+package com.deliverer.deliverer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelivererApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelivererApplication.class, args);
+	}
+
+}
