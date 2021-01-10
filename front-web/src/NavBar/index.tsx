ecd19634fc475e608452f4acf209b8113d650ffc
jujuneos <lcjuniorconceicao@gmail.com>
2021-01-10 20:24:37 -0300

    Adicionando rotas e StepsHeader

--- front-web/src/NavBar/index.tsx
+++ front-web/src/NavBar/index.tsx
@@ -1,11 +1,12 @@
 import './styles.css'
 import { ReactComponent as Logo } from './Logo.svg';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <nav className="main-navbar">
             <Logo />
-            <a href="home" className="logo-text">JR Deliverer</a>
+            <Link to="/" className="logo-text">JR Deliverer</Link>
         </nav>
     )
 }
